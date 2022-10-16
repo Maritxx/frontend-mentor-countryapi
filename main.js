efc872a5d13c9b2518845d7a0259bcae28218534
Marit de Vries <Maritdevries1@live.nl>
2022-10-16 15:41:46 +0200

    added extra data to be displayed in createCountryItem function

--- main.js
+++ main.js
@@ -16,11 +16,29 @@ const displayCountries = async function() {
 //Function that creates new LI for every country.
 const createCountryItem = function(country) {
     const countryList = document.getElementById("country-list");
+    const newCountryItem = document.createElement("li");
 
-    const newLi = document.createElement("li");
-    newLi.innerText = country.name.common;
+    const countryFlag = document.createElement("img");
+    countryFlag.src = country.flags.svg;
 
-    countryList.appendChild(newLi);
+    const countryName = document.createElement("h2");
+    countryName.innerText = country.name.common;
+
+    const countryPopulation = document.createElement("span")
+    countryPopulation.innerHTML = "<b>Population: </b>" + country.population;
+
+    const countryRegion = document.createElement("span")
+    countryRegion.innerHTML = "<b>Region: </b>" + country.region;
+
+    const countryCapital = document.createElement("span");
+    countryCapital.innerHTML = "<b>Capital: </b>" + country.capital;
+
+    countryList.appendChild(newCountryItem);
+    newCountryItem.appendChild(countryFlag);
+    newCountryItem.appendChild(countryName);
+    newCountryItem.appendChild(countryPopulation);
+    newCountryItem.appendChild(countryRegion);
+    newCountryItem.appendChild(countryCapital);
 }
 
 
